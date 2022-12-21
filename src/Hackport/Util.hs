a221597d4ec5efc647e86c256cc3ca7991084b3a
hololeap <hololeap@users.noreply.github.com>
2022-12-21 11:58:09 -0700

    Hackport/Util.hs: Add keys to hackport remote
    Signed-off-by: hololeap <hololeap@users.noreply.github.com>

--- src/Hackport/Util.hs
+++ src/Hackport/Util.hs
@@ -39,7 +39,30 @@ withHackportContext callback = do
     (GlobalEnv verbosity _ _, _) <- ask
     overlayPath <- getOverlayPath
     defaultConf <- liftIO DCC.commentSavedConfig
-    let flags = DCC.savedGlobalFlags defaultConf
+    let flags = (addKeys (DCC.savedGlobalFlags defaultConf))
+                { DCG.globalCacheDir = DSS.Flag "/mnt/development/git/gentoo-haskell/.hackport"
+                , DCG.globalLogsDir = DSS.NoFlag
+                }
     control
       $ \runInIO -> DCG.withRepoContext verbosity (traceShowId flags)
       $ runInIO . (callback <=< restoreM)
+
+-- | Unfortunately, 'DCC.commentSavedConfig' removes the keys and they need
+-- | to be added back.
+addKeys :: DCG.GlobalFlags -> DCG.GlobalFlags
+addKeys flags =
+    let repos = DCG.globalRemoteRepos flags
+    in flags { DCG.globalRemoteRepos = DUN.overNubList (map addKeysRemote) repos }
+  where
+    addKeysRemote :: DCT.RemoteRepo -> DCT.RemoteRepo
+    addKeysRemote r = r { DCT.remoteRepoRootKeys = defaultHackageRemoteRepoKeys }
+
+defaultHackageRemoteRepoKeys :: [String]
+defaultHackageRemoteRepoKeys =
+    [ "fe331502606802feac15e514d9b9ea83fee8b6ffef71335479a2e68d84adc6b0",
+      "1ea9ba32c526d1cc91ab5e5bd364ec5e9e8cb67179a471872f6e26f0ae773d42",
+      "2c6c3627bd6c982990239487f1abd02e08a02e6cf16edb105a8012d444d870c3",
+      "0a5c7ea47cd1b15f01f5f51a33adda7e655bc0f0b0615baa8e271f4c3351e21d",
+      "51f0161b906011b52c6613376b1ae937670da69322113a246a09f807c62f6921"
+    ]
+
